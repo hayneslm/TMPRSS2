a4aa845e9f945c09fd2cf44cf535f0aad6ace1c8
Laura Haynes <hayneslm@umich.edu>
2021-06-13 13:37:13 -0400

    Code working

--- Filter_TMPRSS2_AA_data.R
+++ Filter_TMPRSS2_AA_data.R
@@ -1,4 +1,4 @@
-install.packages(c("tidyverse"))
+install.packages("tidyverse")
 library(tidyverse)
 library(readr)
 library(scales)
@@ -53,6 +53,10 @@ Input_TMPRSS2_filtered = Input_TMPRSS2 %>% filter(baseMean > 100, padj <= 0.05)
 Input_uPA_filtered = Input_uPA %>% filter(baseMean > 100, padj <= 0.05)
 uPA_TMPRSS2_filtered = uPA_TMPRSS2 %>% filter(baseMean > 100, padj <= 0.05)
 
+write_tsv(Input_TMPRSS2_filtered,"Input_TMPRSS2_filtered.xls")
+write_tsv(Input_uPA_filtered, "Input_uPA_filtered.xls")
+write_tsv(uPA_TMPRSS2_filtered,"uPA_TMPRSS2_filtered.xls")
+
 #Combine TMPRSS2 and uPA
 T2_uPA_joined = full_join(Input_TMPRSS2_filtered, Input_uPA_filtered, 
                           by = "Variant") %>% na.omit() %>% 
@@ -66,4 +70,23 @@ T2_uPA_joined = full_join(Input_TMPRSS2_filtered, Input_uPA_filtered,
   select(Variant, Amplicon_TMPRSS2, Amplicon_uPA, baseMean, TMPRSS2_log2, 
          TMPRSS2_padj, uPA_log2, uPA_padj, type)
   
+T2_preferred = T2_uPA_joined %>% filter(uPA_log2<=0, TMPRSS2_log2>0)
+write_tsv(T2_preferred,"T2_preferred.xls")
 
+T2_sigvsNOT = left_join(Input_TMPRSS2_filtered, Input_uPA, by = 'Variant') %>% 
+  na.omit() %>% 
+  rename(TMPRSS2_log2 = log2FoldChange.x,
+         uPA_log2 = log2FoldChange.y,
+         TMPRSS2_padj = padj.x, 
+         uPA_padj = padj.y,
+         baseMean = baseMean.x,
+         type = type.x,
+         Amplicon_TMPRSS2 = Amplicon.x, Amplicon_uPA = Amplicon.y) %>% 
+  select(Variant, Amplicon_TMPRSS2, Amplicon_uPA, baseMean, TMPRSS2_log2, 
+         TMPRSS2_padj, uPA_log2, uPA_padj, type) %>% 
+  filter(TMPRSS2_log2>0,uPA_padj>0.05)
+  
+all_T2_preferred = rbind(T2_preferred,T2_sigvsNOT)
+
+  
+  
